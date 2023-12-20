89a8469bfffd305b9a0c443ed12b8a27326d526c
Andrew <drew.yatskevich@gmail.com>
2023-12-21 01:03:19 +0300

    Establish PostgreSQL connection

--- tech_point/tech_point/settings.py
+++ tech_point/tech_point/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = "tech_point.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": os.getenv("POSTGRES_DB", default=BASE_DIR / "db.sqlite3"),
+        "USER": os.getenv("POSTGRES_USER", default="admin"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="PASSWORD"),
+        "HOST": os.getenv("DB_HOST", default="localhost"),
+        "PORT": os.getenv("DB_PORT", default="5432"),
     }
 }
 
