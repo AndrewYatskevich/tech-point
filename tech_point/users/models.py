7b3f884a63d44690be3723a35c5ffdabefc422f1
Andrew <drew.yatskevich@gmail.com>
2023-12-22 23:49:52 +0300

    Create CustomUser model

---
+++ tech_point/users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from supply_chain.models import Supplier
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+    workplace = models.ForeignKey(
+        Supplier, on_delete=models.CASCADE, related_name="employees"
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
