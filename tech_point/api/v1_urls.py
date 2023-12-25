b0b5f64b0f9819d786d672f6983e828e0789d816
Andrew <drew.yatskevich@gmail.com>
2023-12-26 02:56:03 +0300

    Set up OpenAPI documentation

--- tech_point/api/v1_urls.py
+++ tech_point/api/v1_urls.py
@@ -1,4 +1,9 @@
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
@@ -16,6 +21,17 @@ router_v1.register(r"supply-chain-links", SupplyChainLinkViewSet)
 router_v1.register(r"products", ProductViewSet)
 
 urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("auth/sign-up/", UserSignUp.as_view(), name="sign-up"),
     path("auth/sign-in/", obtain_auth_token, name="sign-in"),
     path("", include(router_v1.urls)),
